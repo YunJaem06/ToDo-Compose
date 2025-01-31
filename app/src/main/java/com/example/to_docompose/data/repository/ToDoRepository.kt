9bebbef67ccb437ec55a1dd963df3a022ab367b9
yunjaem06 <yunjae0616@gmail.com>
2025-01-31 12:02:37 +0900

    [feat] Dagger Hilt repository

---
+++ app/src/main/java/com/example/to_docompose/data/repository/ToDoRepository.kt
@@ -0,0 +1,37 @@
+package com.example.to_docompose.data.repository
+
+import com.example.to_docompose.data.ToDoDao
+import com.example.to_docompose.data.models.TodoTask
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class ToDoRepository @Inject constructor(private val toDoDao: ToDoDao) {
+
+    val getAllTasks : Flow<List<TodoTask>> = toDoDao.getAllTasks()
+    val sortByLowPriority : Flow<List<TodoTask>> = toDoDao.sortByLowPriority()
+    val sortByHighPriority : Flow<List<TodoTask>> = toDoDao.sortByHighPriority()
+
+    fun getSelectedTask(taskId : Int) : Flow<TodoTask> {
+        return toDoDao.getSelectedTask(taskId = taskId)
+    }
+
+    suspend fun addTask(todoTask: TodoTask) {
+        toDoDao.addTask(todoTask)
+    }
+
+    suspend fun updateTask(todoTask: TodoTask) {
+        toDoDao.updateTask(todoTask)
+    }
+
+    suspend fun deleteTask(todoTask: TodoTask) {
+        toDoDao.deleteTask(todoTask)
+    }
+
+    suspend fun deleteAllTask() {
+        toDoDao.deleteAllTasks()
+    }
+
+    fun searchDatabase(searchQuery: String) : Flow<List<TodoTask>> {
+        return toDoDao.searchDatabase(searchQuery = searchQuery)
+    }
+}
