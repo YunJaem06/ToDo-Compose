3c3a2dce364e0cffd1c321a42d7b6adbd64329d0
yunjaem06 <yunjae0616@gmail.com>
2025-06-14 12:50:40 +0900

    [feat] search view 추가

--- app/src/main/java/com/example/to_docompose/ui/screens/list/ListAppBar.kt
+++ app/src/main/java/com/example/to_docompose/ui/screens/list/ListAppBar.kt
@@ -1,26 +1,40 @@
 package com.example.to_docompose.ui.screens.list
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.to_docompose.R
+import com.example.to_docompose.data.models.Priority
 import com.example.to_docompose.ui.theme.topAppBarBackgroundColor
 import com.example.to_docompose.ui.theme.topAppBarContentColor
 
 @Composable
 fun ListAppBar() {
-    DefaultListAppBar()
+    DefaultListAppBar(
+        onSearchClicked = {}
+    )
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun DefaultListAppBar() {
+fun DefaultListAppBar(
+    onSearchClicked: () -> Unit
+) {
     TopAppBar(
         title = {
             Text(text = "Tasks", color = MaterialTheme.colorScheme.topAppBarContentColor)
+        },
+        actions = {
+            ListAppBarActions(onSearchClicked = onSearchClicked)
         },
         colors = TopAppBarDefaults.smallTopAppBarColors(
             containerColor = MaterialTheme.colorScheme.topAppBarBackgroundColor
@@ -29,7 +43,35 @@ fun DefaultListAppBar() {
 }
 
 @Composable
+fun ListAppBarActions(
+    onSearchClicked: () -> Unit
+) {
+    SearchAction(onSearchClicked = onSearchClicked)
+}
+
+@Composable
+fun SearchAction(
+    onSearchClicked: () -> Unit
+) {
+    IconButton(onClick = { onSearchClicked() }
+    ) {
+        Icon(imageVector = Icons.Filled.Search,
+            contentDescription = stringResource(R.string.search_action),
+            tint = MaterialTheme.colorScheme.topAppBarContentColor)
+    }
+}
+
+@Composable
+fun SortAction(
+    onSortClicked: (Priority) -> Unit
+) {
+
+}
+
+@Composable
 @Preview
 private fun DefaultListAppBarPreview() {
-    DefaultListAppBar()
+    DefaultListAppBar(
+        onSearchClicked = {}
+    )
 }
